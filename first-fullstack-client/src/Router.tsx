a0d991733816c67c716c2077218ec4cd66c6ac72
Eric Jiang <eric.jiang@pnnl.gov>
2024-02-26 12:02:15 -0800

    create App Router

---
+++ first-fullstack-client/src/Router.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+// import Login from "./components/Login";
+// import Signup from "./components/Signup";
+// import PostList from "./components/PostList";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      {/* <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/" component={PostList} exact /> */}
+      {/* Add more routes as needed */}
+    </Routes>
+  );
+};
+
+export default AppRouter;
