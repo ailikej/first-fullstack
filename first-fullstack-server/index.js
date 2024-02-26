8dfab4be047ae135f052771bf619c3b1d195d492
Eric Jiang <eric.jiang@pnnl.gov>
2024-02-26 13:38:27 -0800

    resolve CORS issue in the backend

--- first-fullstack-server/index.js
+++ first-fullstack-server/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors'); // Add this line
 const authRoutes = require('./routes/authRoutes'); // Adjust the path based on your project structure
 const postRoutes = require('./routes/postRoutes'); // Ensure this path matches your file structure
 
 
 const app = express();
+
+// This will allow all origins
+app.use(cors());
+
 const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON bodies
