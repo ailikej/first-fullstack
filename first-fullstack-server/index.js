b8983885e2ffbb0f506d3b19fc592646af1f6e2c
Eric Jiang <eric.jiang@pnnl.gov>
2024-02-26 11:18:47 -0800

    add postRouts to index

--- first-fullstack-server/index.js
+++ first-fullstack-server/index.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const authRoutes = require('./routes/authRoutes'); // Adjust the path based on your project structure
+const postRoutes = require('./routes/postRoutes'); // Ensure this path matches your file structure
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +18,7 @@ app.get('/', (req, res) => {
 
 // Use the auth routes with a base path of /api/auth
 app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes); // Now postRoutes is defined
 
 // Start the server
 app.listen(PORT, () => {
