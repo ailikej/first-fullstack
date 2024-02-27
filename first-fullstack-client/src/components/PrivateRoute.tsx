9cd179adaa86fc447969403f78936870e23944cd
Eric Jiang <eric.jiang@pnnl.gov>
2024-02-26 21:46:02 -0800

    use Reudx state

--- first-fullstack-client/src/components/PrivateRoute.tsx
+++ first-fullstack-client/src/components/PrivateRoute.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { selectUserToken } from "../features/userSlice";
 
 interface PrivateRouteProps {
   children: React.ReactNode;
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const token = localStorage.getItem("token");
+  const token = useSelector(selectUserToken);
 
   return (
     <React.Fragment>
