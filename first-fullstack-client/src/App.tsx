0f2bb6fa9b3098c16e8de44a68d9d752481a2d18
Eric Jiang <eric.jiang@pnnl.gov>
2024-02-26 22:03:51 -0800

    move token checker to App, so Routers can seperate this concern

--- first-fullstack-client/src/App.tsx
+++ first-fullstack-client/src/App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "./Router";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setUserToken } from "./features/userSlice";
 
 function App() {
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(setUserToken({ token }));
+    }
+    setIsCheckingAuth(false);
+  }, [dispatch]);
+
+  if (isCheckingAuth) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
       <div className="bg-red-100 min-h-screen">
--- first-fullstack-client/src/Router.tsx
+++ first-fullstack-client/src/Router.tsx
@@ -1,32 +1,11 @@
-import { useEffect, useState } from "react";
 import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import PostList from "./components/PostList";
 import Signup from "./components/Signup";
 import SinglePostPage from "./components/SinglePostPage";
 import PrivateRoute from "./components/PrivateRoute";
-import { useDispatch } from "react-redux";
-import { setUserToken } from "./features/userSlice";
 
 const AppRouter = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(true); // Add a loading state
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      dispatch(setUserToken({ token })); // Rehydrate your app's state with the token
-    } else {
-      navigate("/login");
-    }
-    setIsLoading(false); // Set loading to false after handling token
-  }, [dispatch, navigate]);
-
-  if (isLoading) {
-    return <div>Loading...</div>; // Or any other loading indicator
-  }
-
   return (
     <Routes>
       <Route
@@ -48,7 +27,6 @@ const AppRouter = () => {
       />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/after-login" element={<Navigate to="/" replace />} />
     </Routes>
   );
 };
