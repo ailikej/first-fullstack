2707a544e2c751601cc91b60bfffadb567dfec66
Eric Jiang <eric.jiang@pnnl.gov>
2024-02-26 16:29:26 -0800

    refactor Router

--- first-fullstack-client/src/Router.tsx
+++ first-fullstack-client/src/Router.tsx
@@ -3,14 +3,16 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import PostList from "./components/PostList";
 import Signup from "./components/Signup";
+import SinglePostPage from "./components/SinglePostPage";
 
 const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/" element={<PostList />} />
+      <Route path="/posts" element={<Navigate to="/" replace />} />
+      <Route path="/posts/:id" element={<SinglePostPage />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<PostList />} />
-      {/* Redirect user to PostList (or any other page) after login */}
       <Route path="/after-login" element={<Navigate to="/" replace />} />
     </Routes>
   );
